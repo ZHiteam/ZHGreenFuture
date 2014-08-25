fc3b33e4f8a7a86eae580b55481c2fcd9787655a
zzzfly121 <zzzfly121@gmail.com>
2014-08-25 22:42:04 +0800

    first init

---
+++ ZHGreenFuture/ZHGreenFuture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZHGreenFuture
+//
+//  Created by admin on 14-8-25.
+//  Copyright (c) 2014年 ZHiteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZHGreenFuture/ZHGreenFuture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZHGreenFuture
+//
+//  Created by admin on 14-8-25.
+//  Copyright (c) 2014年 ZHiteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
