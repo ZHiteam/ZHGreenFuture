b737a7d6bd03d67d881751d2ac6c3e3c6638e18c
zzzfly121 <zzzfly121@gmail.com>
2014-11-04 10:31:51 +0800

    update

--- ZHGreenFuture/ZHGreenFuture/Sources/Classes/Controller/ZHHomePage/Model/ZHDetailModel.m
+++ ZHGreenFuture/ZHGreenFuture/Sources/Classes/Controller/ZHHomePage/Model/ZHDetailModel.m
@@ -17,7 +17,7 @@
         if ([dict isKindOfClass:[NSDictionary class]]) {
             self.imageURL = [dict objectForKey:@"imageURL"];
             self.title    = [dict objectForKey:@"title"];
-            self.recipeId = [dict objectForKey:@"recipeId"];
+            self.recipeId = VALIDATE_VALUE([dict objectForKey:@"recipeId"]);
             self.imageURL = [self.imageURL greenFutureURLStr];
         }
     }
